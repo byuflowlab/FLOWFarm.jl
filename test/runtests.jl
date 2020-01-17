937df6504cbc15000fc721d13770b4a203183faf
jaredthomas68 <jaredthomas68@gmail.com>
2020-01-17 16:16:28 -0700

    adding tests for Jensen

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
-using FlowFarm
+import FlowFarm; const ff = FlowFarm
 using Test
 
-@testset "FlowFarm.jl" begin
+@testset "Wake Models" begin
     # Write your own tests here.
+    
+    @testset "Jensen Model" begin
+        coord = ff.Coord(0.0,0.0,0.0)
+        rotor_diameter = 40.0
+        hub_height = 90.0
+        aI = 1.0/3.0
+        gamma = 0.0
+        yaw = 0.0
+        ct = 0.7 #TODO handle ct and axial induction appropriately
+        
+        turbine = ff.Turbine(coord, rotor_diameter, hub_height, aI, gamma, yaw, ct)
+        
+        deflection = [0.0, 0.0]
+
+        model = ff.Jensen(0.1)
+
+        centerloss40 = 1. - 4.35/8.1
+        centerloss100 = 1. - 5.7/8.1
+
+        @test ff.wake_model([40., 0.0, hub_height], deflection, model, turbine) == centerloss40
+        @test ff.wake_model([100., 0.0, hub_height], deflection, model, turbine) == centerloss100
+
+    end
+    
 end
