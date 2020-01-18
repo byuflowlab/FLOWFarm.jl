b57a3aee227759a5ae6d2fccc8472d97122abc83
jaredthomas68 <jaredthomas68@gmail.com>
2020-01-18 08:21:23 -0700

    completed Jensen top hat model tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,18 +12,41 @@ using Test
         gamma = 0.0
         yaw = 0.0
         ct = 0.7 #TODO handle ct and axial induction appropriately
+
+        alpha = 0.1
         
         turbine = ff.Turbine(coord, rotor_diameter, hub_height, aI, gamma, yaw, ct)
         
         deflection = [0.0, 0.0]
 
-        model = ff.Jensen(0.1)
+        model = ff.Jensen(alpha)
 
         centerloss40 = 1. - 4.35/8.1
         centerloss100 = 1. - 5.7/8.1
 
+        # test no loss upstream (data from Jensen 1983)
+        @test ff.wake_model([-1E-12, 0.0, hub_height], deflection, model, turbine) == 0.0
+
+        # test max loss at turbine (data from Jensen 1983)
+        @test ff.wake_model([0.0, 0.0, hub_height], deflection, model, turbine) == (2. / 3.)
+
+        # test centerline loss 40 diameters downstream (data from Jensen 1983)
         @test ff.wake_model([40., 0.0, hub_height], deflection, model, turbine) == centerloss40
+
+        # test centerline loss 40 diameters downstream (data from Jensen 1983)
         @test ff.wake_model([100., 0.0, hub_height], deflection, model, turbine) == centerloss100
+
+        # test wake diameter 40 meters downstream (data from Jensen 1983)
+        @test ff.wake_model([40., (alpha*40 + rotor_diameter/2.), hub_height], deflection, model, turbine) == centerloss40
+        @test ff.wake_model([40., (alpha*40 + rotor_diameter/2. + 1E-12), hub_height], deflection, model, turbine) == 0.0
+        @test ff.wake_model([40., -(alpha*40 + rotor_diameter/2.), hub_height], deflection, model, turbine) == centerloss40
+        @test ff.wake_model([40., -(alpha*40 + rotor_diameter/2. + 1E-12), hub_height], deflection, model, turbine) == 0.0
+
+        # test wake diameter 100 meters downstream (data from Jensen 1983)
+        @test ff.wake_model([100., (alpha*100. + rotor_diameter/2.), hub_height], deflection, model, turbine) == centerloss100
+        @test ff.wake_model([100., (alpha*100. + rotor_diameter/2. + 1E-12), hub_height], deflection, model, turbine) == 0.0
+        @test ff.wake_model([100., -(alpha*100. + rotor_diameter/2.), hub_height], deflection, model, turbine) == centerloss100
+        @test ff.wake_model([100., -(alpha*100. + rotor_diameter/2. + 1E-12), hub_height], deflection, model, turbine) == 0.0
 
     end
     
