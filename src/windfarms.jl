6b49ac27f96de965de108c4e4787cc2f6bf90a2e
jaredthomas68 <jaredthomas68@gmail.com>
2020-03-02 08:53:25 -0700

    working on top level structs

--- src/windfarms.jl
+++ src/windfarms.jl
@@ -1,5 +1,7 @@
 
 abstract type AbstractWindFarmModel end
+abstract type AbstractWindFarmProblem end
+abstract type AbstractModelSet end
 
 struct WindFarm{AF,AS} <: AbstractWindFarmModel
     
@@ -24,5 +26,42 @@ struct SingleWindFarmState{TI,AF,AI} <: AbstractWindFarmModel
     sorted_turbine_index::AI
     turbine_inflow_velcities::AF
     turbine_generators_powers::AF
+
+end
+
+"""
+    WindFarmProblemDescription(windfarm, windresource, windfarmstates)
+
+Container for objects defining a wind farm problem
+
+# Arguments
+- `wind_farm::Array{WindFarm}(1)`: contains windturbine coordinates and definitions
+- `wind_resource::Array{AbstracWindResource}(1)`: wind resource description
+- `wind_farm_states::Array{SingleWindFarmState}(Nstates)`: contains turbine coordinates operational states
+"""
+struct WindFarmProblemDescription{AFM,AWR,AFS} <: AbstractWindFarmProblem
+   
+    wind_farm::AFM
+    wind_resource::AWR
+    wind_farm_states::AFS
+
+end
+
+
+"""
+    WindFarmModelSet(wakedeficitmodel, wake_deflection_model, wake_combination_model, ti_model, wind_shear_model)
+
+Container for objects defining models to use in wind farm calculations
+
+# Arguments
+- `wake_defiict_model::Array{AbstractWakeDeficitModel}(1)`: contains a struct defining the desired wake deficit model
+- `wake_deflection_model::Array{AbstractWakeDeflectionModel}(1)`: contains a struct defining the desired wake deflection model
+- `wake_combination_model::Array{AbstractWakeCombinationModel}(1)`: contains a struct defining the desired wake combination model
+- `ti_model::Array{AbstractWakeDeflectionModel}(1)`: contains a struct defining the desired wake deflection model
+- `wind_shear_model::Array{SingleWindFarmState}(Nstates)`: contains turbine coordinates operational states
+"""
+struct WindFarmModelSet{ADTM,ADNM,ACM,ATIM,ASM} <: AbstractModelSet
+
+
 
 end
