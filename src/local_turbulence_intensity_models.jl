de2a667481736be26d0f1a895a7a557ea7e91787
jaredthomas68 <jaredthomas68@gmail.com>
2020-03-10 15:38:40 -0600

    changes for top level set up and struct refactoring

--- src/local_turbulence_intensity_models.jl
+++ src/local_turbulence_intensity_models.jl
@@ -3,20 +3,3 @@ abstract type AbstractLocalTurbulenceIntensityModel end
 struct NoLocalTI{TF} <: AbstractLocalTurbulenceIntensityModel
     ct::TF
 end
-
-# struct InterpolatedCt{TI, AF} <: AbstractThrustCoefficientModel
-#     interpolation_order::TI
-#     wind_speeds::AF
-#     cts::AF
-# end
-
-function calculate_ct(model::ConstantCt)
-    return model.ct
-end
-
-# function calculate_ct(wind_speed, model::InterpolatedCt)
-
-    
-
-
-# end
--- src/wind_resources.jl
+++ src/wind_resources.jl
@@ -1,12 +1,25 @@
 abstract type AbstractWindResourceModel end
 
-struct DiscretizedWindResource{AF, TF} <: AbstractWindResourceModel
+"""
+    DiscritizedWindResource(wind_directions, wind_speeds, wind_probabilities, measurement_heights, air_density, ti_model, wind_shear_model)
+
+    Struct defining a wind resource
+
+# Arguments
+- `wind_directions::Array{Float}(Nstates)`: contains an array of wind directions corresponding to each wind farm state
+- `wind_speeds::Array{Float}(Nstates)`: contains an array of wind speeds corresponding to each wind farm state
+- `wind_probabilities::Array{Float}(Nstates)`: contains an array of probabilities corresponding to each wind farm state
+- `measurement_heights::Array{Float}(Nstates)`: contains an array of measurement heights corresponding to each wind farm state
+- `air_density::Array{Float}(1): length 1 array containing the air density
+- `wind_shear_model::Array{AbstractWindShearModel}(1)`: contains a struct defining the desired turbulence intensity model
+"""
+struct DiscretizedWindResource{AF, ASM} <: AbstractWindResourceModel
     
     wind_directions::AF
     wind_speeds::AF
     wind_probabilities::AF
     measurement_heights::AF
-    shear_exponent::TF
-    air_density::TF
+    air_density::AF
+    wind_shear_model::ASM
 
 end
