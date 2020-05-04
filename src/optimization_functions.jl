a6af835a8509813bc3aacf5874cc2ef839d22abf
PJ Stanley <stanley_andrewpj@yahoo.com>
2020-05-04 14:35:15 -0500

    boundary constraints

--- src/optimization_functions.jl
+++ src/optimization_functions.jl
@@ -32,16 +32,88 @@ end
 
 
 """
-    circle_boundary(center_x,center_y,radius,turbine_x,turbine_y)
+    circle_boundary(center,radius,turbine_x,turbine_y)
 
-calculate the
+calculate the distance from each turbine to a circular boundary. Negative means the
+turbine is inside the boundary
 
 # Arguments
-- `center_x::Float`: circular boundary center x
-- `center_y::Float`: circular boundary center y
+- `center::Float`: circular boundary center [x,y]
 - `radius::Float`: circulat boundary radius
 - `turbine_x::Array{Float}`: turbine x locations
 - `turbine_y::Array{Float}`: turbine y locations
 """
-function circle_boundary(center_x,center_y,radius,turbine_x,turbine_y)
+function circle_boundary(center,radius,turbine_x,turbine_y)
+        nturbines = length(turbine_x)
+        boundary_vec = zeros(nturbines)
+        for i = 1:nturbines
+                boundary_vec[i] = sqrt((center[1]-turbine_x[i])^2 + (center[2]-turbine_y[i])^2) - radius
+        end
+        return boundary_vec
 end
+
+
+"""
+    windfarm_boundary(boundary_vertices,boundary_normals,turbine_x,turbine_y)
+
+calculate the distance from each turbine to a circular boundary. Negative means the
+turbine is inside the boundary
+
+# Arguments
+- `boundary_vertices::Array{Float,2}`: vertices of the convex hull CCW in order s.t.
+        boundaryVertices[i] -> first point of face for unit_normals[i]
+- `boundary_normals::Array{Float,2}`: unit normal vector for each boundary face
+        CCW where boundaryVertices[i] is the first point of the corresponding face
+- `turbine_x::Array{Float}`: turbine x locations
+- `turbine_y::Array{Float}`: turbine y locations
+"""
+function windfarm_boundary(boundary_vertices,boundary_normals,turbine_x,turbine_y)
+        nturbines = length(turbine_x)
+        nVertices = size(boundary_vertices)[1]
+        # initialize array to hold distances from each point to each face
+        face_distance = zeros(nturbines, nVertices)
+
+        # loop through points and find distance to each face
+        for i = 1:nturbines
+                # determine if point is inside or outside of each face, and distance from each face
+                for j = 1:nVertices
+                        # define the vector from the point of interest to the first point of the face
+                        pa = [boundary_vertices[j, 1]-turbine_x[i], boundary_vertices[j, 2]-turbine_y[i]]
+                        # find perpendicular distance from point to current surface (vector projection)
+                        d_vec = sum(pa .* boundary_normals[j,:]) .* boundary_normals[j,:]
+                        # calculate the sign of perpendicular distance from point to current face (- is inside, + is outside)
+                        face_distance[i, j] = -sum(d_vec .* boundary_normals[j,:])
+                end
+        end
+        return face_distance
+end
+
+
+function calculate_boundary(vertices)
+
+    # find the points that actually comprise a convex hull
+    hull = ConvexHull(list(vertices))
+
+    # keep only vertices that actually comprise a convex hull and arrange in CCW order
+    vertices = vertices[hull.vertices]
+
+    # get the real number of vertices
+    nVertices = vertices.shape[0]
+
+    # initialize normals array
+    unit_normals = np.zeros([nVertices, 2])
+
+    # determine if point is inside or outside of each face, and distance from each face
+    for j in range(0, nVertices):
+
+        # calculate the unit normal vector of the current face (taking points CCW)
+        if j < nVertices - 1:  # all but the set of point that close the shape
+            normal = np.array([vertices[j+1, 1]-vertices[j, 1],
+                               -(vertices[j+1, 0]-vertices[j, 0])])
+            unit_normals[j] = normal/np.linalg.norm(normal)
+        else:   # the set of points that close the shape
+            normal = np.array([vertices[0, 1]-vertices[j, 1],
+                               -(vertices[0, 0]-vertices[j, 0])])
+            unit_normals[j] = normal/np.linalg.norm(normal)
+
+    return vertices, unit_normals
