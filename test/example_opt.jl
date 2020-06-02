282a2d888dc4e19831f00d6905f311384b5e2f70
jaredthomas68 <jaredthomas68@gmail.com>
2020-06-02 10:14:32 -0600

    completed example_opt.jl

--- test/example_opt.jl
+++ test/example_opt.jl
@@ -1,21 +1,21 @@
 using Snopt
 using DelimitedFiles 
-using PyPlot
+import PyPlot
 using ForwardDiff
 using DiffResults
 
 using Snopt
 import ForwardDiff
 
-# function boundary_wrapper(x)
-#     global boundary_vertices
-#     global boundary_normals
-#     global scale
-#     nturbines = Int(length(x)/2)
-#     turbine_x = x[1:nturbines].*scale
-#     turbine_y = x[nturbines+1:end].*scale
-#     return ff.windfarm_boundary(boundary_vertices,boundary_normals,turbine_x,turbine_y) .* 10.0
-# end
+function boundary_wrapper(x)
+    global boundary_center
+    global boundary_radius
+    global scale
+    nturbines = Int(length(x)/2)
+    turbine_x = x[1:nturbines].*scale
+    turbine_y = x[nturbines+1:end].*scale
+    return ff.circle_boundary(boundary_center, boundary_radius, turbine_x, turbine_y)
+end
 
 function spacing_wrapper(x)
     global rotor_diameter
@@ -59,16 +59,20 @@ end
 function wind_farm_opt(x)
 
     spacing_con = spacing_wrapper(x)
-    ds_dx = ForwardDiff.jacobian(spacing_wrapper,x)
+    ds_dx = ForwardDiff.jacobian(spacing_wrapper, x)
 
-    # boundary_con = boundary_wrapper(x)
-    # db_dx = ForwardDiff.jacobian(boundary_wrapper,x)
+    boundary_con = boundary_wrapper(x)
+    db_dx = ForwardDiff.jacobian(boundary_wrapper, x)
 
-    c = [spacing_con]#;boundary_con]
-    dcdx = [ds_dx]#;db_dx]
+    c = [spacing_con; boundary_con]
+    dcdx = [ds_dx; db_dx]
 
     AEP = -aep_wrapper(x)[1]
     dAEP_dx = -ForwardDiff.jacobian(aep_wrapper,x)
+    # println("AEP ", AEP)
+    # println(dAEP_dx)
+    # println(c)
+    # println(dcdx)
 
     fail = false
     return AEP, c, dAEP_dx, dcdx, fail
@@ -94,17 +98,23 @@ boundary_radius = 300.0
 include("./model_sets/model_set_6.jl")
 x = [copy(turbine_x);copy(turbine_y)]./scale
 
-println("start: ", aep_wrapper(x))
+println("start: ", aep_wrapper(x)[1])
 
 turbine_x = copy(x[1:nturbines]).*scale
 turbine_y = copy(x[nturbines+1:end]).*scale
 area = zeros(length(turbine_x)) .+ pi*(rotor_diameter[1]/2.0)^2
+h1 = 0
 for i = 1:length(turbine_x)
-    plt.gcf().gca().add_artist(plt.Circle((turbine_x[i],turbine_y[i]), rotor_diameter/2.0, fill=false, color="C0"))
+    if i==1
+        # println("HEREHE")
+        h1 = plt.gcf().gca().add_artist(plt.Circle((turbine_x[i],turbine_y[i]), rotor_diameter[1]/2.0, fill=false,color="C0",label="Start"))
+    else
+        plt.gcf().gca().add_artist(plt.Circle((turbine_x[i],turbine_y[i]), rotor_diameter[1]/2.0, fill=false,color="C0"))
+    end
 end
 
-lb = zeros(length(x)) .-boundary_radius
-ub = zeros(length(x)) .+boundary_radius
+lb = zeros(length(x)) .- boundary_radius
+ub = zeros(length(x)) .+ boundary_radius
 options = Dict{String, Any}()
 options["Derivative option"] = 1
 options["Verify level"] = 3
@@ -115,7 +125,9 @@ options["Print file"] = "print.out"
 
 t1 = time()
 xopt, fopt, info = snopt(wind_farm_opt, x, lb, ub, options)
-println("Finished in : ", time()-t1, " (s)")
+t2 = time()
+clkt = t2-t2
+println("Finished in : ", clkt, " (s)")
 # println("xopt: ", xopt)
 # println("fopt: ", fopt)
 println("info: ", info)
@@ -125,11 +137,19 @@ println("finish: ", aep_wrapper(xopt))
 turbine_x = copy(xopt[1:nturbines]).*scale
 turbine_y = copy(xopt[nturbines+1:end]).*scale
 
+h2 = 0
 for i = 1:length(turbine_x)
-    plt.gcf().gca().add_artist(plt.Circle((turbine_x[i],turbine_y[i]), rotor_diameter/2.0, fill=false,color="C1"))
+    if i==1
+        h2 = plt.gcf().gca().add_artist(plt.Circle((turbine_x[i],turbine_y[i]), rotor_diameter[1]/2.0, fill=false,color="C1",label="End", linestyle="--"))
+    else
+        plt.gcf().gca().add_artist(plt.Circle((turbine_x[i],turbine_y[i]), rotor_diameter[1]/2.0, fill=false,color="C1", linestyle="--"))
+    end
+    
 end
 plt.gcf().gca().add_artist(plt.Circle((boundary_center[1],boundary_center[2]), boundary_radius, fill=false,color="C2"))
 
 axis("square")
 xlim(-boundary_radius-200,boundary_radius+200)
 ylim(-boundary_radius-200,boundary_radius+200)
+plt.legend([h1, h2])
+plt.show()
