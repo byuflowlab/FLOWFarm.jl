91c8b7876c5fbcefdba41e85b357c0125859c67f
Nick Baker <nicholas.f.baker@byu.edu>
2020-07-15 14:09:29 -0600

    SC slurm

--- test/ieacs4splinedbash.sh
+++ test/ieacs4splinedbash.sh
@@ -2,13 +2,9 @@
 
 #SBATCH --time=01:00:00   # walltime
 #SBATCH --ntasks=288   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=1024M   # memory per CPU core
 #SBATCH -J "IEACS4 Splined Boundary"   # job name
-#SBATCH --mail-user=nicholas.f.baker@gmail.com   # email address
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
 #export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
