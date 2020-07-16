0617eb2b26692cbdefc663388069f1c57ed33abd
wesleyjholt <64283549+wesleyjholt@users.noreply.github.com>
2020-07-16 07:33:57 -0600

    add Ipopt example with WEC

---
+++ test/example_opt_Ipopt_2_WEC.jl
@@ -0,0 +1,263 @@
+using Ipopt
+using DelimitedFiles 
+using PyPlot
+import ForwardDiff
+
+### uses a Julia interface to the Ipopt nonlinear solver
+
+# set up boundary constraint wrapper function
+function boundary_wrapper(x, params)
+    # include relevant globals
+    params.boundary_vertices
+    params.boundary_normals
+
+    # get number of turbines
+    nturbines = Int(length(x)/2)
+    
+    # extract x and y locations of turbines from design variables vector
+    turbine_x = x[1:nturbines]
+    turbine_y = x[nturbines+1:end]
+
+    # get and return boundary distances
+    return ff.ray_trace_boundary(boundary_vertices, boundary_normals, turbine_x, turbine_y)
+end
+
+# set up spacing constraint wrapper function
+function spacing_wrapper(x, params)
+    # include relevant globals
+    params.rotor_diameter
+
+    # get number of turbines
+    nturbines = Int(length(x)/2)
+
+    # extract x and y locations of turbines from design variables vector
+    turbine_x = x[1:nturbines]
+    turbine_y = x[nturbines+1:end]
+
+    # get and return spacing distances
+    return 2.0*rotor_diameter[1] .- ff.turbine_spacing(turbine_x,turbine_y)
+end
+
+# set up objective wrapper function
+function aep_wrapper(x, params)
+    # include relevant globals
+    params.turbine_z
+    params.rotor_diameter
+    params.hub_height
+    params.turbine_yaw
+    params.ct_models
+    params.generator_efficiency
+    params.cut_in_speed
+    params.cut_out_speed
+    params.rated_speed
+    params.rated_power
+    params.windresource
+    params.power_models
+    params.model_set
+    params.rotor_points_y
+    params.rotor_points_z
+    params.obj_scale
+
+    # get number of turbines
+    nturbines = Int(length(x)/2)
+
+    # extract x and y locations of turbines from design variables vector
+    turbine_x = x[1:nturbines] 
+    turbine_y = x[nturbines+1:end]
+
+    # calculate AEP
+    AEP = obj_scale*ff.calculate_aep(turbine_x, turbine_y, turbine_z, rotor_diameter,
+                hub_height, turbine_yaw, ct_models, generator_efficiency, cut_in_speed,
+                cut_out_speed, rated_speed, rated_power, windresource, power_models, model_set,
+                rotor_sample_points_y=rotor_points_y,rotor_sample_points_z=rotor_points_z)
+    
+    # return the objective as an array
+    return [AEP]
+end
+
+# objective function
+function obj(x) 
+    return -aep_wrapper(x)[1]
+end
+
+# constraint function
+function con(x, g)
+    # calculate spacing constraint value
+    spacing_con = spacing_wrapper(x)
+
+    # calculate boundary constraint
+    boundary_con = boundary_wrapper(x)
+
+    # combine constaint values and jacobians into overall constaint value and jacobian arrays
+    g[:] = [spacing_con; boundary_con]
+end
+
+# objective gradient function
+function obj_grad(x, grad_f)
+    grad_f[:] = -ForwardDiff.jacobian(aep_wrapper,x)
+end
+
+# constraint gradients function
+function con_grad(x, mode, rows, cols, values)
+    if mode == :Structure
+        # report the sparcity structure of the jacobian
+        for i = 1:prob.m
+            rows[(i-1)*prob.n+1:i*prob.n] = i*ones(Int,prob.n)
+            cols[(i-1)*prob.n+1:i*prob.n] = 1:prob.n
+        end
+    else
+        # calculate spacing constraint jacobian
+        ds_dx = ForwardDiff.jacobian(spacing_wrapper, x)
+
+        # calculate boundary constraint jacobian
+        db_dx = ForwardDiff.jacobian(boundary_wrapper, x)
+
+        # combine constaint jacobians into overall constaint jacobian arrays
+        for i = 1:prob.m
+            for j = 1:prob.n
+                values[(i-1)*prob.n+j] = [ds_dx; db_dx][i, j]
+            end
+        end
+    end
+end
+
+# import model set with wind farm and related details
+include("./model_sets/model_set_6.jl")
+
+# scale objective to be between 0 and 1
+obj_scale = 1E-11
+
+# set wind farm boundary parameters
+boundary_vertices = ([0 0; 1 0; 1 .75; .75 .75; .75 1; 0 1] .- .5).*500 # Utah-shape boundary
+boundary_normals = [0 1.0; -1 0; 0 -1; -1 0; 0 -1; 1 0]
+
+# set globals for use in wrapper functions
+struct params_struct2{}
+    model_set
+    rotor_points_y
+    rotor_points_z
+    turbine_z
+    ambient_ti
+    rotor_diameter
+    boundary_vertices
+    boundary_normals
+    obj_scale
+    hub_height
+    turbine_yaw
+    ct_models
+    generator_efficiency
+    cut_in_speed
+    cut_out_speed
+    rated_speed
+    rated_power
+    windresource
+    power_models
+end
+
+params = params_struct2(model_set, rotor_points_y, rotor_points_z, turbine_z, ambient_ti, 
+    rotor_diameter, boundary_vertices, boundary_normals, obj_scale, hub_height, turbine_yaw, 
+    ct_models, generator_efficiency, cut_in_speed, cut_out_speed, rated_speed, rated_power, 
+    windresource, power_models)
+
+# initialize design variable array
+x = [copy(turbine_x);copy(turbine_y)]
+x[1] += 5
+global x
+
+# report initial objective value
+println("starting objective value: ", aep_wrapper(x, params)[1])
+
+# add initial turbine location to plot
+for i = 1:length(turbine_x)
+    plt.gcf().gca().add_artist(plt.Circle((turbine_x[i],turbine_y[i]), rotor_diameter[1]/2.0, fill=false,color="C0"))
+end
+
+# get number of design variables
+n_designvariables = length(x)
+
+# get number of constraints
+function numberofspacingconstraints(nturb)
+    # calculates number of spacing constraints needed for given number of turbines
+    ncon = 0
+    for i = 1:nturb-1; ncon += i; end
+    return ncon
+end
+n_spacingconstraints = numberofspacingconstraints(nturbines)
+n_boundaryconstraints = length(boundary_wrapper(x, params))
+n_constraints = n_spacingconstraints + n_boundaryconstraints
+
+# set general lower and upper bounds for design variables
+lb = ones(n_designvariables) * -Inf
+ub = ones(n_designvariables) * Inf
+
+# set lower and upper bounds for constraints
+lb_g = ones(n_constraints) * -Inf
+ub_g = zeros(n_constraints)
+
+# generate wrapper function surrogates
+spacing_wrapper(x) = spacing_wrapper(x, params)
+aep_wrapper(x) = aep_wrapper(x, params)
+boundary_wrapper(x) = boundary_wrapper(x, params)
+
+# create the problem
+prob = createProblem(n_designvariables, lb, ub, n_constraints, lb_g, ub_g, n_designvariables*n_constraints, 0,
+    obj, con, obj_grad, con_grad)
+addOption(prob, "hessian_approximation", "limited-memory")
+
+# set up for WEC optimization
+wec_steps = 2
+wec_max = 3.0
+wec_end = 1.0
+wec_values = collect(LinRange(wec_max, wec_end, wec_steps))
+println(wec_values)
+info = fill("",wec_steps)
+
+# run and time optimization
+t1t = time()
+for i =1:length(wec_values)
+    global x
+    global xopt
+    println("Running with WEC = ", wec_values[i])
+    params.model_set.wake_deficit_model.wec_factor[1] = wec_values[i]
+    prob.x = x
+
+    t1 = time()
+    status = solveProblem(prob)
+    t2 = time()
+    clk = t2-t1
+    xopt = prob.x
+    fopt = prob.obj_val
+    info = Ipopt.ApplicationReturnStatus[status]
+    # print optimization results
+    println("Finished in : ", clk, " (s)")
+    println("info: ", info)
+    println("end objective value: ", -fopt)
+    println("locations ", x[1:5])
+    println("locations opt ", xopt[1:5])
+    x = deepcopy(xopt)
+end
+t2t = time()
+clkt = t2t - t1t
+# print optimization results
+println("Finished in : ", clkt, " (s)")
+println("info: ", info)
+println("end objective value: ", aep_wrapper(xopt)[1])
+
+# extract final turbine locations
+turbine_x = copy(xopt[1:nturbines])
+turbine_y = copy(xopt[nturbines+1:end])
+
+# add final turbine locations to plot
+for i = 1:length(turbine_x)
+    plt.gcf().gca().add_artist(plt.Circle((turbine_x[i],turbine_y[i]), rotor_diameter[1]/2.0, fill=false,color="C1", linestyle="--")) 
+end
+
+# add wind farm boundary to plot
+plt.gcf().gca().plot([boundary_vertices[:,1];boundary_vertices[1,1]],[boundary_vertices[:,2];boundary_vertices[1,2]], color="C2")
+
+# set up and show plot
+axis("square")
+xlim(minimum(boundary_vertices) - (maximum(boundary_vertices)-minimum(boundary_vertices))/5, maximum(boundary_vertices) + (maximum(boundary_vertices)-minimum(boundary_vertices))/5)
+ylim(minimum(boundary_vertices) - (maximum(boundary_vertices)-minimum(boundary_vertices))/5, maximum(boundary_vertices) + (maximum(boundary_vertices)-minimum(boundary_vertices))/5)
+plt.show()
+savefig("opt_plot")
