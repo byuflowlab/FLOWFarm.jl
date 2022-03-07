96602a8f9972e9ba325fb538a8a5f769bdd21e37
James Cutler <cutlerjames36@gmail.com>
2022-03-07 12:56:34 -0700

    Finished adding tilt functions to general models I changes the way we calculate locx, locy, and locz

--- src/general_models.jl
+++ src/general_models.jl
@@ -373,6 +373,123 @@ function turbine_velocities_one_direction(turbine_x, turbine_y, turbine_z, rotor
     end
 end
 
+"""
+    turbine_velocities_one_direction_tilt(turbine_x, turbine_y, turbine_z, rotor_diameter, hub_height, turbine_tilt,
+    sorted_turbine_index, ct_model, rotor_sample_points_y, rotor_sample_points_z, wind_resource,
+    model_set::AbstractModelSet; wind_farm_state_id=1)
+
+Calculates the wind speed at a given point for a given state
+
+# Arguments
+- `turbine_x::Array{TF,nTurbines}`: turbine east-west locations in the state 
+    reference frame
+- `turbine_y::Array{TF,nTurbines}`: turbine north-south locations in the state 
+    reference frame
+- `turbine_z::Array{TF,nTurbines}`: turbine base height in the state reference frame
+- `rotor_diameter::Array{TF,nTurbines}`: turbine rotor diameters
+- `hub_height::Array{TF,nTurbines}`: turbine hub heights
+- `turbine_tilt::Array{TF,nTurbines}`: turbine tilt for the given wind direction in 
+    radians
+- `sorted_turbine_index::Array{TF,nTurbines}`: turbine sorted order upstream to downstream 
+    for given state
+- `ct_model::AbstractThrustCoefficientModel`: defines how the thrust coefficient changes 
+    with state etc
+- rotor_sample_points_y::Array{TF,N}`: horizontal wind location of points to sample across 
+    the rotor swept area when calculating the effective wind speed for the wind turbine. 
+    Points are centered at the hub (0,0) and scaled by the radius (1=tip of blades) 
+- rotor_sample_points_z::Array{TF,N}`: vertical wind location of points to sample across the 
+    rotor swept area when calculating the effective wind speed for the wind turbine. Points
+    are centered at the hub (0,0) and scaled by the radius (1=tip of blades)
+- `wind_resource::DiscretizedWindResource`: wind resource discreption (directions, speeds, 
+    frequencies, etc)
+- `model_set::AbstractModelSet`: defines wake-realated models to be used in analysis
+- `wind_farm_state_id::Int`: index to correct state to use from wind resource provided.
+    Defaults to 1
+"""
+function turbine_velocities_one_direction_tilt(turbine_x, turbine_y, turbine_z, rotor_diameter, hub_height, turbine_tilt,
+                    sorted_turbine_index, ct_model, rotor_sample_points_y, rotor_sample_points_z, wind_resource,
+                    model_set::AbstractModelSet; wind_farm_state_id=1, velocity_only=true)
+    
+    # get number of turbines and rotor sample point
+    n_turbines = length(turbine_x)
+    n_rotor_sample_points = length(rotor_sample_points_y)
+
+    # initialize correct array types
+    arr_type = promote_type(typeof(turbine_x[1]),typeof(turbine_y[1]),typeof(turbine_z[1]),typeof(rotor_diameter[1]),
+                            typeof(hub_height[1]),typeof(turbine_tilt[1]))
+
+    # initialize arrays
+    turbine_velocities = zeros(arr_type, n_turbines)
+    turbine_ct = zeros(arr_type, n_turbines)
+    turbine_ai = zeros(arr_type, n_turbines)
+    turbine_local_ti = zeros(arr_type, n_turbines)
+
+    # loop over all turbines
+    for d=1:n_turbines
+
+        # get index of downstream turbine
+        downwind_turbine_id = Int(sorted_turbine_index[d])
+
+        # initialize downstream wind turbine velocity to zero
+        # println("start array: ", turbine_velocities[downwind_turbine_id])
+        # wind_turbine_velocity = typeof(turbine_velocities[downwind_turbine_id])(0.0)
+        wind_turbine_velocity = 0.0
+        # turbine_velocities[downwind_turbine_id] = 0.0
+
+        # loop over all rotor sample points to approximate the effective inflow velocity
+        for p=1:n_rotor_sample_points
+
+            # scale rotor sample point coordinate by rotor diameter (in rotor hub ref. frame)
+            local_rotor_sample_point_y = rotor_sample_points_y[p]*0.5*rotor_diameter[downwind_turbine_id]
+            local_rotor_sample_point_z = rotor_sample_points_z[p]*0.5*rotor_diameter[downwind_turbine_id]
+
+            # put rotor sample points in wind direction coordinate system, and account for yaw
+            locx = turbine_x[downwind_turbine_id] .+ local_rotor_sample_point_y*sin(turbine_tilt[downwind_turbine_id])
+            locy = turbine_y[downwind_turbine_id] .+ local_rotor_sample_point_y
+            locz = turbine_z[downwind_turbine_id] .+ hub_he5ight[downwind_turbine_id] .+ local_rotor_sample_point_z*cos(turbine_tilt[downwind_turbine_id])
+
+            # calculate the velocity at given point
+            point_velocity_with_shear = point_velocity_tilt(locx, locy, locz, turbine_x, turbine_y, turbine_z, turbine_tilt, turbine_ct, turbine_ai,
+                                    rotor_diameter, hub_height, turbine_local_ti, sorted_turbine_index, turbine_velocities,
+                                    wind_resource, model_set,
+                                    wind_farm_state_id=wind_farm_state_id, downwind_turbine_id=downwind_turbine_id)
+
+            # add sample point velocity to turbine velocity to be averaged later
+            wind_turbine_velocity += point_velocity_with_shear
+
+        end
+
+        # final velocity calculation for downstream turbine (average equally across all points)
+        wind_turbine_velocity /= n_rotor_sample_points
+
+        turbine_velocities[downwind_turbine_id] = deepcopy(wind_turbine_velocity)
+
+        # update thrust coefficient for downstream turbine
+        turbine_ct[downwind_turbine_id] = calculate_ct(turbine_velocities[downwind_turbine_id], ct_model[downwind_turbine_id])
+
+        # update axial induction for downstream turbine
+        turbine_ai[downwind_turbine_id] = _ct_to_axial_ind_func(turbine_ct[downwind_turbine_id])
+
+        # get local turbulence intensity for this wind state
+        ambient_ti = wind_resource.ambient_tis[wind_farm_state_id]
+        
+        # update local turbulence intensity for downstream turbine
+        turbine_local_ti[downwind_turbine_id] = calculate_local_ti(turbine_x, turbine_y, ambient_ti, rotor_diameter, hub_height, turbine_tilt, turbine_local_ti, sorted_turbine_index,
+                            turbine_velocities, turbine_ct, model_set.local_ti_model; turbine_id=downwind_turbine_id, tol=1E-6)
+
+        # println("local ti turb 9: ", turbine_local_ti[downwind_turbine_id])
+    end
+
+    # df = DataFrame(ID=1:n_turbines, V=turbine_velocities, TI=turbine_local_ti, CT=turbine_ct)
+    # CSV.write("internaldata.txt", df)
+
+    if velocity_only
+        return turbine_velocities 
+    else
+        return turbine_velocities, turbine_ct, turbine_ai, turbine_local_ti
+    end
+end
+
 function turbine_velocities_one_direction(x, turbine_z, rotor_diameter, hub_height, turbine_yaw,
     sorted_turbine_index, ct_model, rotor_sample_points_y, rotor_sample_points_z, wind_resource,
     model_set::AbstractModelSet; wind_farm_state_id=1, velocity_only=true)
@@ -561,3 +678,75 @@ function calculate_flow_field(xrange, yrange, zrange,
         wind_resource, wind_farm_state_id=wind_farm_state_id)
 
 end
+
+function calculate_flow_field_tilt(xrange, yrange, zrange,
+    model_set::AbstractModelSet, turbine_x, turbine_y, turbine_z, turbine_tilt, turbine_ct, turbine_ai,
+    rotor_diameter, hub_height, turbine_local_ti, sorted_turbine_index, wtvelocities,
+    wind_resource; wind_farm_state_id=1)
+
+    xlen = length(xrange)
+    ylen = length(yrange)
+    zlen = length(zrange)
+    npoints = xlen*ylen*zlen
+    point_velocities = zeros(npoints)
+    point_velocities = reshape(point_velocities, (zlen, ylen, xlen))
+
+    # rotate to direction frame for velocity calculations
+    rot_tx, rot_ty = rotate_to_wind_direction(turbine_x, turbine_y, wind_resource.wind_directions[wind_farm_state_id])
+
+    # sort the turbines
+    sorted_turbine_index = sortperm(rot_tx)
+
+    for zi in 1:zlen
+        for yi in 1:ylen
+            for xi in 1:xlen
+                locx = xrange[xi]
+                locy = yrange[yi]
+                locz = zrange[zi]
+                locx, locy = rotate_to_wind_direction(locx, locy, wind_resource.wind_directions[wind_farm_state_id])
+
+                point_velocities[zi, yi, xi] = point_velocity_tilt(locx, locy, locz, rot_tx, rot_ty, turbine_z, turbine_tilt, turbine_ct, turbine_ai,
+                    rotor_diameter, hub_height, turbine_local_ti, sorted_turbine_index, wtvelocities,
+                    wind_resource, model_set,
+                    wind_farm_state_id=wind_farm_state_id, downwind_turbine_id=0)
+
+            end
+        end
+    end
+
+    
+
+    # if zlen == 1
+    #     return point_velocities[1,1:ylen,1:xlen]
+    # elseif ylen == 1
+    #     return point_velocities[1:zlen,1,1:xlen]
+    # elseif xlen == 1
+    #     return point_velocities[1:zlen,1:ylen,1]
+    # else
+    return point_velocities[1:zlen,1:ylen,1:xlen]
+    # end
+
+end
+
+function calculate_flow_field_tilt(xrange, yrange, zrange,
+    model_set::AbstractModelSet, turbine_x, turbine_y, turbine_z, turbine_tilt,
+    rotor_diameter, hub_height, ct_models, rotor_sample_points_y, rotor_sample_points_z,
+    wind_resource; wind_farm_state_id=1)
+
+    # rotate to direction frame for velocity calculations
+    rot_tx, rot_ty = rotate_to_wind_direction(turbine_x, turbine_y, wind_resource.wind_directions[wind_farm_state_id])
+
+    # sort the turbines
+    sorted_turbine_index = sortperm(rot_tx)
+
+    turbine_velocities, turbine_ct, turbine_ai, turbine_local_ti = turbine_velocities_one_direction_tilt(rot_tx, rot_ty, turbine_z, rotor_diameter, hub_height, turbine_tilt,
+    sorted_turbine_index, ct_models, rotor_sample_points_y, rotor_sample_points_z, wind_resource,
+    model_set, wind_farm_state_id=wind_farm_state_id, velocity_only=false)
+
+    return calculate_flow_field(xrange, yrange, zrange,
+        model_set, turbine_x, turbine_y, turbine_z, turbine_yaw, turbine_ct, turbine_ai,
+        rotor_diameter, hub_height, turbine_local_ti, sorted_turbine_index, turbine_velocities,
+        wind_resource, wind_farm_state_id=wind_farm_state_id)
+
+end
+
