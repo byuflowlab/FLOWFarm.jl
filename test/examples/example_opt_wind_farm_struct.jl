86b80b14ca54fa529489af0b79d3daab9c7dc214
BTV25 <70768698+BTV25@users.noreply.github.com>
2025-02-18 11:55:12 -0700

    Update docs

--- test/examples/example_opt_wind_farm_struct.jl
+++ test/examples/example_opt_wind_farm_struct.jl
@@ -1,35 +1,37 @@
 using SNOW
 using DelimitedFiles
 import FLOWFarm; const ff = FLOWFarm
+using SparseArrays
 
 function opt!(g,df,dg,x,farm,spacing_struct,boundary_struct)
 
     # calculate spacing constraint value and jacobian
-    ff.calculate_spacing!(spacing_struct.spacing_vec,x,farm,spacing_struct)
-    ds(a,x) = ff.calculate_spacing!(a,x,farm,spacing_struct)
-    ForwardDiff.jacobian!(spacing_struct.spacing_jacobian,ds,spacing_vec,x,spacing_struct.forward_cfg)
+    ff.calculate_spacing_jacobian!(spacing_struct,x)
 
     # calculate boundary constraint and jacobian
-    ff.calculate_boundary!(boundary_struct.boundary_vec,x,farm,boundary_struct)
-    db(a,x) = ff.calculate_boundary!(a,x,farm,boundary_struct)
-    ForwardDiff.jacobian!(boundary_struct.boundary_jacobian,db,boundary_vec,x,boundary_struct.forward_cfg)
+    ff.calculate_boundary_jacobian!(boundary_struct,x)
 
     # combine constaint values and jacobians into overall constaint value and jacobian arrays
     c = [spacing_struct.spacing_vec; boundary_struct.boundary_vec]
-    g[:] .= c[:]
+    g[:] .= ((c[:]))
+    dg[:] = (([spacing_struct.jacobian; boundary_struct.jacobian]))
 
-    dcdx = [dropzeros(sparse(spacing_struct.spacing_jacobian)); dropzeros(sparse(boundary_struct.boundary_jacobian))]
-    dg[:] .= dcdx[:]
+    # calculate AEP and gradient
+    ff.calculate_aep_gradient!(farm,x)
 
-    # calculate the objective function and jacobian (negative sign in order to maximize AEP)
-    AEP = -ff.calculate_aep(x,farm)
-
-    aep(x) = ff.calculate_aep(x,farm)
-    ForwardDiff.gradient!(farm.AEP_gradient,df,x,farm.duals.forward_cfg)
-
-    df[:] .= -farm.AEP_gradient
+    AEP = -farm.AEP[1]
+    df[:] .= ((-farm.AEP_gradient))
 
     return AEP
+end
+
+function update_function(farm,x,r)
+    n = length(farm.turbine_x)
+    x .= ((x .* 2.0) .- 1.0) .* r
+    farm.turbine_x .= x[1:n]
+    farm.turbine_y .= x[n+1:end]
+    x .= ((x ./ r) .+ 1.0) ./ 2.0
+    return nothing
 end
 
 # import model set with wind farm and related details
@@ -39,25 +41,32 @@ include("../model_sets/model_set_9_38turb_round_farm.jl")
 boundary_center = [0.0,0.0]
 boundary_radius = 1225.8227848101264
 
+x = ((turbine_x ./ boundary_radius) .+ 1.0) ./ 2
+y = ((turbine_y ./ boundary_radius) .+ 1.0) ./ 2
+
 # initialize design variable array
-x0 = [copy(turbine_x);copy(turbine_y)]
+x0 = [deepcopy(x);deepcopy(y)]
 
-function update_function(farm,x,n)
-    ff.update_turbine_x!(farm,x[1:n])
-    ff.update_turbine_y!(farm,x[n+1:end])
-    return nothing
-end
-n = length(turbine_x)
-f(farm,x) = update_function(farm,x,n)
+num_turbines = n = length(turbine_x)
 
+# set up boundary function
+f(farm,x) = update_function(farm,x,boundary_radius)
 
+# build wind farm struct
+    # opt_x and opt_y are set to true to optimize turbine x and y positions
 farm = ff.build_wind_farm_struct(x0,turbine_x,turbine_y,turbine_z,hub_height,turbine_yaw,
             rotor_diameter,ct_models,generator_efficiency,cut_in_speed,cut_out_speed,rated_speed,
-            rated_power,windresource,power_models,model_set;update_function=f,opt_x=true,opt_y=true)
+            rated_power,windresource,power_models,model_set,f;input_type="ForwardDiff",opt_x=true,opt_y=true)
 
-spacing_struct = ff.build_spacing_struct(x,n,1,1)
-boundary_function(a,x,y) = ff.circle_boundary!(a,boundary_centerboundary_radius,x,y)
-boundary_struct = ff.build_boundary_struct(x,n,1,boundary_function)
+spacing = 2.0 * rotor_diameter[1] # 2 rotor diameters spacing between turbines
+scaling = 1.0/rotor_diameter[1]^2 # scaling factor for spacing constraint
+spacing_struct = ff.build_spacing_struct(x0,num_turbines,spacing,scaling,f)
+
+boundary_function(a,x,y) = ff.circle_boundary!(a,boundary_center,boundary_radius,x,y) # boundary function
+n_constraints = num_turbines # number of boundary constraints
+scaling = 1.0/boundary_radius^3.5 # scaling factor for boundary constraint
+boundary_struct = ff.build_boundary_struct(x0,num_turbines,n_constraints,scaling,boundary_function,f)
+
 
 ip_options = Dict(
     "max_iter" => 30,
@@ -72,7 +81,7 @@ lx = zeros(length(x0))
 ux = ones(length(x0))
 ng = length(lg)
 
+x_start = deepcopy(x0)
+
 opt!(g,df,dg,x) = opt!(g,df,dg,x,farm,spacing_struct,boundary_struct)
 xopt, fopt, info = minimize(opt!, x0, ng, lx, ux, lg, ug, options)
-
-println("Done")
